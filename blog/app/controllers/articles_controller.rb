5f6ed26fee4e6efbe762893495b949884b1d5ea0
Michael Lawrence <mdlawrence@me.com>
2015-03-26 18:17:08 -0400

    Class Update as of March 26, 2015

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "mdl", password: "secret", except: [:index, :show]
+
   def new
     @article = Article.new
   end
